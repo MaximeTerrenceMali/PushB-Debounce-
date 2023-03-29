9ce9c490c0aeb40f1c3bd4a2c7b9edbc5aff25fc
MaximeTerrenceMali <114182448+MaximeTerrenceMali@users.noreply.github.com>
2023-03-29 16:48:09 -0400

    Update PushB.h

--- PushB.h
+++ PushB.h
@@ -1,1 +1,21 @@
+#ifndef PushB_H
+#define PushB_H
 
+#include<Arduino.h>
+
+class PushB{
+
+private:
+int pin;
+
+public:
+LED(){}       // default construtor
+LED(int pin); //the constructor
+
+void init();  //to initialize the pin as output
+void on();    //to turn the LED on
+void off();   //to turn the LED off
+
+};
+
+#endif
