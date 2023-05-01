b301832d83be63077cafa46d6d8d802925ac25de
MaximeTerrenceMali <114182448+MaximeTerrenceMali@users.noreply.github.com>
2023-04-30 22:43:47 -0400

    Update PushB.h

--- PushB.h
+++ PushB.h
@@ -1,21 +1,17 @@
-#ifndef PushB_H
-#define PushB_H
+#ifndef PB_h // Header guard to avoid multiple inclusion of header file
+#define PB_h 
 
-#include<Arduino.h>
+#include <Arduino.h> // Include Arduino header file
 
-class PushB{
-
-private:
-int pin;
-
-public:
-LED(){}       // default construtor
-LED(int pin); //the constructor
-
-void init();  //to initialize the pin as output
-void on();    //to turn the LED on
-void off();   //to turn the LED off
-
+class PB { // Definition of the PB class
+  public:
+    PB(int pin); // Constructor implementation for creating a PB instance with specified pin
+    bool isPressed(); // Method implementation for checking if the button is pressed
+  private:
+    int _pin; 
+    bool _lastState; // Record of final button state
+    unsigned long _lastChangeTime; // The time when the final button state last modified
+    unsigned long _debounceDelay; // The amount of time dellay needed to get rid of debounce
 };
 
-#endif
+#endif // indicate the ending of the piece of code to compile
